381802f82cf364a7d9ab4193c52c7b1efede0f44
mistyclench <mistyclench99@gmail.com>
2024-12-10 23:54:58 +0000

    change react version for vercel / netlify deploy

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -21,7 +21,7 @@ export default async function Dashboard() {
           </article>
         </div>
         <div className="w-1/4 p-4 gap-2">
-          <article className="bg-white rounded-lg shadow p-3">
+          <article className="bg-white rounded-lg shadow p-3 text-2xl">
             {/* Article content */}
             <p className={`antialiased text-gray-800 md:leading-normal`}>
               Total Initial shares
@@ -47,8 +47,8 @@ export default async function Dashboard() {
             <h1 className="text-7xl text-gray-400">1000</h1>
           </article>
         </div>
-  </div>
-  <div className="flex flex-wrap">
+    </div>
+    <div className="flex flex-wrap">
     <div className="w-2/3 p-4 gap-2">
     <article className="bg-white rounded-lg shadow p-3 text-2xl">
     <table className="table-auto border-spacing-x w-full">
@@ -94,6 +94,32 @@ export default async function Dashboard() {
     </tbody>
     </table>
         </article>
-        </div>  </div>
+        </div>  
+    </div>
+    <div className="w-full p-4 gap-2">
+    <article className="bg-white rounded-lg shadow p-3 text-2xl">
+    <table className="table-auto border-spacing-x w-full">
+  <thead>
+    <tr>
+      <th className="float-left">Company Name</th>
+      <th className="">Initial Amount</th>
+      <th>Shares</th>
+    </tr>
+  </thead>
+  <tbody>
+    {shares.map ((share) => {
+      return(
+        <tr key={share.id}>
+      <td>{share.companyName}</td>
+      <td>{share.sharePrice}</td>
+      <td>{share.shares}</td>
+        </tr>
+      )
+    } )}
+    </tbody>
+    </table>
+        </article>
+        </div>
+
   </main>
   )}
