059aa474641b72a5be958e511e176cd0e4862d22
mistyclench <mistyclench99@gmail.com>
2024-12-11 00:42:11 +0000

    Lint fix

--- src/app/authProvider.tsx
+++ src/app/authProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 import {KindeProvider} from "@kinde-oss/kinde-auth-nextjs";
 
-export const AuthProvider = ({children}) => {
+export const AuthProvider = (children: React.ReactNode) => {
   return <KindeProvider>{children}</KindeProvider>;
 };
